639041d4d9f0a0a28ef83449b49a2b5578f7e719
timotius kevin <slackware.04@gmail.com>
2011-10-14 02:05:56 +0700

    sesi 4 dengan comment

---
+++ sesi4/soal1/Latihan.java
@@ -0,0 +1,9 @@
+public class Latihan
+{
+	public static void main(String[] args)
+	{
+		int age = 21; //deklarasi variabel age dengan nilai 21
+
+System.out.println(age > 50? "You are old" : "Your are young"); //menampilkan "you are old" jika variabel age > 50; "you are young" jika < 50
+}
+}
