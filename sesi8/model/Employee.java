cf26cc91b8dfd07de3ec9a02bc9f3ae48a287d7e
timotius kevin <slackware.04@gmail.com>
2011-11-25 00:33:16 +0700

    sesi 8

---
+++ sesi8/model/Employee.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Employee extends Person
+{
+	private String nip;
+
+	public String getNip()
+	{
+	return nip;
+	}
+
+	public void setNip(String nip)
+	{
+		this.nip = nip;
+	}
+
+}
